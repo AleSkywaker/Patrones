d030cdd5c09af0162d0dfca62bfb84daed537f2b
Alex Colombo <uricaine@hotmail.com>
2019-02-01 22:26:49 +0100

    refactor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var leadself = 'Me: ';
-var	leadcomputer = "PC: ";
-		aSaid= ["This is a Cyber Chat"],
-		msgYes = "Yes, that's a great idea.",
-		msgNo = "No, that must be a mistake.",
-		aSassyStuff = ["Like mold on books, grow myths on history.",
-									"She moved like a poem and smiled like a sphinx.",
-									"As long as we don’t die, this is gonna be one hell of a story.",
-									"She laughed, and the desert sang.",
-									"You’ve got about as much charm as a dead slug."];
+var yo = 'Me: ';
+var	computer = "PC: ";
+var loDicho = ["This is a Cyber Chat"];
+var	msgSi = "Yes, that's a great idea.";
+var	msgNo = "No, that must be a mistake.";
+var	frases = [	"Like mold on books, grow myths on history.",
+				"She moved like a poem and smiled like a sphinx.",
+				"As long as we don’t die, this is gonna be one hell of a story.",
+				"She laughed, and the desert sang.",
+				"You’ve got about as much charm as a dead slug."];
 
 
 
