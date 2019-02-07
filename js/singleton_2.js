41d0beec733bf5e1c7ceb68a637958f0bec999b3
Alex Colombo <uricaine@hotmail.com>
2019-02-07 20:16:08 +0100

    singleton

--- js/singleton_2.js
+++ js/singleton_2.js
@@ -1,4 +1,23 @@
 const alumnos = {
 
-    
+    // todos los alumnos
+    listaAlumnos: [],
+
+
+    //obtener alumnos
+    get: function(alumno){
+        alumno
+    },
+
+    //Crear alumno
+
+    crear: function(){
+
+    },
+
+    //listado alumnos
+    listado:function(){
+
+    }
+
 } 
