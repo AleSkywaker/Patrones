700bbc20dd918f353f70ec4d8c50f7061d961634
Alex Colombo <uricaine@hotmail.com>
2019-02-07 20:25:01 +0100

    singleton

--- js/singleton_2.js
+++ js/singleton_2.js
@@ -11,13 +11,27 @@ const alumnos = {
 
     //Crear alumno
 
-    crear: function(){
+    crear: function(datos){
+        console.log(datos)
 
     },
 
     //listado alumnos
     listado:function(){
+        return this.listaAlumnos;
 
     }
 
 } 
+
+const alumno = {
+    nombre:"Alex",
+    edad: 20
+}
+const alumno2 = {
+    nombre:"Juan",
+    edad: 28
+}
+
+alumnos.crear(alumno)
+alumnos.crear(alumno2)
