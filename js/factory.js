48b18ce22e18d0c0fe30a6433debd995f68adcec
Alex Colombo <uricaine@hotmail.com>
2019-02-08 19:26:40 +0100

    factory

--- js/factory.js
+++ js/factory.js
@@ -1,5 +1,15 @@
 function ConstructorSitios(){
     this.crearElemento = function(texto,tipo){
         let html;
+
+        if(tipo === 'input'){
+            html = new InputHTML(texto);
+        }else if(tipo === 'img') {
+            html = new ImageHTML(texto)
+        }else if(tipo === 'h1'){
+            html = new HeadingHTML(texo)
+        }else{
+            
+        }
     }
 }
