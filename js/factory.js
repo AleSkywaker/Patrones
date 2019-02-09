d3351542aad3741b067cfdd68b2f65de02019dcc
Alex Colombo <uricaine@hotmail.com>
2019-02-09 09:09:46 +0100

    factory

--- js/factory.js
+++ js/factory.js
@@ -7,9 +7,33 @@ function ConstructorSitios(){
         }else if(tipo === 'img') {
             html = new ImageHTML(texto)
         }else if(tipo === 'h1'){
-            html = new HeadingHTML(texo)
+            html = new HeadingHTML(texto)
         }else if(tipo === 'p'){ 
             html = new ParrafoHTML(texto)
         }
+        return html
     }
 }
+
+const InputHTML = function(texto){
+    this.texto = texto
+}
+const ImageHTML = function(texto){
+    this.texto = texto
+}
+const HeadingHTML = function(texto){
+    this.texto = texto
+}
+const ParrafoHTML = function(texto){
+    this.texto = texto
+}
+
+const sitioWeb = new ConstructorSitios();
+
+//Almacenar elementos
+const elementosWeb = [];
+elementosWeb.push(sitioWeb.crearElemento('Bienvenidos', 'h1'))
+elementosWeb.push(sitioWeb.crearElemento('Alex', 'input'))
+
+
+console.log(elementosWeb)
