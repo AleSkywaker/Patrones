27e486ceaa8488ff6f1ca97676c2323123984cb9
Alex Colombo <uricaine@hotmail.com>
2019-02-09 09:20:04 +0100

    factory

--- js/factory.js
+++ js/factory.js
@@ -1,43 +1,55 @@
-function ConstructorSitios(){
-    this.crearElemento = function(texto,tipo){
-        let html;
+function ConstructorSitios() {
+  this.crearElemento = function(texto, tipo) {
+    let html;
 
-        if(tipo === 'input'){
-            html = new InputHTML(texto);
-        }else if(tipo === 'img') {
-            html = new ImageHTML(texto)
-        }else if(tipo === 'h1'){
-            html = new HeadingHTML(texto)
-        }else if(tipo === 'p'){ 
-            html = new ParrafoHTML(texto)
-        }
-
-        html.tipo = tipo;
-        return html
+    if (tipo === "input") {
+      html = new InputHTML(texto);
+    } else if (tipo === "img") {
+      html = new ImageHTML(texto);
+    } else if (tipo === "h1") {
+      html = new HeadingHTML(texto);
+    } else if (tipo === "p") {
+      html = new ParrafoHTML(texto);
     }
+
+    html.tipo = tipo;
+
+    html.mostrar = function() {
+      const elemento = documento.crearElemento(this.tipo);
+
+      if (this.tipo === "input") {
+        elemento.setAttribute("placeholder", this.tipo);
+      } else if (this.tipo === "img") {
+        elemento.setAttribute("src", this.tipo);
+      } else {
+          elemento.setAttribute('text', this.tipo)
+      }
+    };
+
+    return html;
+  };
 }
 
-const InputHTML = function(texto){
-    this.texto = texto
-}
-const ImageHTML = function(texto){
-    this.texto = texto
-}
-const HeadingHTML = function(texto){
-    this.texto = texto
-}
-const ParrafoHTML = function(texto){
-    this.texto = texto
-}
+const InputHTML = function(texto) {
+  this.texto = texto;
+};
+const ImageHTML = function(texto) {
+  this.texto = texto;
+};
+const HeadingHTML = function(texto) {
+  this.texto = texto;
+};
+const ParrafoHTML = function(texto) {
+  this.texto = texto;
+};
 
 const sitioWeb = new ConstructorSitios();
 
 //Almacenar elementos
 const elementosWeb = [];
-elementosWeb.push(sitioWeb.crearElemento('Bienvenidos', 'h1'))
-elementosWeb.push(sitioWeb.crearElemento('Contacto', 'input'))
-elementosWeb.push(sitioWeb.crearElemento('Conoce mas sobre nosotros', 'p'))
-elementosWeb.push(sitioWeb.crearElemento('logo.svg', 'img'))
+elementosWeb.push(sitioWeb.crearElemento("Bienvenidos", "h1"));
+elementosWeb.push(sitioWeb.crearElemento("Contacto", "input"));
+elementosWeb.push(sitioWeb.crearElemento("Conoce mas sobre nosotros", "p"));
+elementosWeb.push(sitioWeb.crearElemento("logo.svg", "img"));
 
-
-console.log(elementosWeb)
+console.log(elementosWeb);
