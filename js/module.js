c62218c9247427bb4185669e2eb50c9912a8ca4c
Alex Colombo <uricaine@hotmail.com>
2019-02-13 19:47:34 +0100

    module

--- js/module.js
+++ js/module.js
@@ -2,11 +2,18 @@ const comprarEntrada = (function() {
 	//Privado
 	let evento = 'Conferencia JS 2019';
 
+	const adquirirEntrada = () => {
+		const elemento = document.createElement('p');
+		elemento.textContent = `Comprando entrada para : ${evento}`;
+		document.querySelector('#app').appendChild(elemento);
+	};
+
 	//Publico
 	return {
 		evento,
 		mostrarEntrada: function() {
 			console.log('Ha comprado una entrada para :', evento);
+			adquirirEntrada();
 		}
 	};
 })();
